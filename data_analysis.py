ba4963c05c77a07d6a4c83bf3bc868f1e704ec2e
Paulo Arevalo <parevalo@bu.edu>
2018-12-12 16:14:42 -0500

    Initial commit

---
+++ data_analysis.py
@@ -0,0 +1,33 @@
+"""
+Analysis data function used to download and process some termperature time
+series from Berkeley Earth.
+"""
+
+import numpy as np
+import requests
+
+def generate_url(location):
+    url=f'http://berkeleyearth.lbl.gov/auto/Regional/TAVG/Text/{location.lower()}-TAVG-Trend.txt'
+    return url
+
+
+def download_data(location):
+    url = generate_url(location)
+    response = requests.get(url)
+    data = np.loadtxt(response.iter_lines(), comments="%")
+    return data
+
+
+def moving_average(data, width):
+    moving_avg = np.full(data.size, np.nan)
+    for i in range(width, data.size - width):
+        moving_avg[i] = np.mean(data[i - width:i + width])
+
+    return moving_avg
+
+
+def test_moving_avg():
+    avg = moving_average(np.ones(10), 2)
+    assert np.all(np.isnan(avg[0:2]))
+    assert np.all(np.isnan(avg[-2:]))
+    assert np.allclose(avg[2:-2] == 1, 1)
