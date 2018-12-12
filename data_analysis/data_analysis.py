2e6ebc5a6ed07d62c79d4482e020a51d537966f9
Paulo Arevalo <parevalo@bu.edu>
2018-12-12 17:13:36 -0500

    Update docstrings

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -7,11 +7,13 @@ import numpy as np
 import requests
 
 def generate_url(location):
+    """Generate download URL for any location"""
     url=f'http://berkeleyearth.lbl.gov/auto/Regional/TAVG/Text/{location.lower()}-TAVG-Trend.txt'
     return url
 
 
 def download_data(location):
+    """Download data for a given location"""
     url = generate_url(location)
     response = requests.get(url)
     data = np.loadtxt(response.iter_lines(), comments="%")
@@ -19,6 +21,10 @@ def download_data(location):
 
 
 def moving_average(data, width):
+    """Calculate a moving average for a given width
+    :param data: Input data
+    :param width: Width in samples
+    """
     moving_avg = np.full(data.size, np.nan)
     for i in range(width, data.size - width):
         moving_avg[i] = np.mean(data[i - width:i + width])
